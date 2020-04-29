10aa9a20413f477b778656bf5170cc6fae776649
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2020-04-30 00:57:39 +0530

    Update output.tf

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -1,11 +1,23 @@
 
+
 output "alb" {
   value = "${data.aws_lb.alb.id}"
 }
+
 output "route" {
   value = "{var.public_hosted_zone}"
 }
-output "targetarn" {
-  value = "{data.aws_lb_target_group.target.arn}"  
+
+output "listenerarn" {
+  value = "${aws_alb_listener.listener.arn}"
 }
+
+output "tg1" {
+  value = "{aws_alb_target_group.target1.id}"  
+}
+
+output "tg2" {
+  value = "{aws_alb_target_group.target2.id}"  
+}
+
 
