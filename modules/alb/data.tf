c90aebd334c023489b2b0c5e636ac807b826a6d6
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2020-04-30 00:56:57 +0530

    Update data.tf

--- modules/alb/data.tf
+++ modules/alb/data.tf
@@ -1,8 +1,12 @@
 
+
+data "aws_acm_certificate" "sslcert" {
+  domain  =  "servermyip.com"
+  types   =  ["AMAZON_ISSUED"]
+  most_recent  =  true
+}
+
 data "aws_lb" "alb" {
  name = var.ALB_NAME
-}
-data "aws_lb_target_group" "target" {
-   name = "alb-target-group2"
 }
 
